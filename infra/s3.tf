2f24191899d2f7888566170b2886a80ac8f53589
i544c <vrc@i544c.me>
2022-12-27 02:36:19 +0900

    fix: Add Cloudfront origin access control

--- infra/s3.tf
+++ infra/s3.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "main" {
     }
     actions = ["s3:GetObject"]
     resources = [
-      "${aws_s3_bucket.main.arn}",
+      aws_s3_bucket.main.arn,
       "${aws_s3_bucket.main.arn}/*"
     ]
     condition {
