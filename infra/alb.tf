6fdf8d0ddb9d2f8307cf0a1c5912a03d655335dc
i544c <vrc@i544c.me>
2023-07-03 19:10:23 +0900

    hotfix: ヘルスチェックはフロントを見るよう修正
    API で確認してたけど、どうもフロントだけ落ちるケースがある模様。
    結果フロントは死んだのにヘルスチェックが通ってしまう。
    
    これは困るのでヘルスチェックを戻す。

--- infra/alb.tf
+++ infra/alb.tf
@@ -37,10 +37,6 @@ resource "aws_lb_target_group" "app" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
-
-  health_check {
-    path = "/api/ping"
-  }
 }
 
 resource "aws_lb_listener" "app" {
