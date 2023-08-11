9b2af9b1489934909ed7a9afc404235746a5f773
i544c <vrc@i544c.me>
2023-08-11 16:50:37 +0900

    feat: Add https listener to alb

--- infra/alb.tf
+++ infra/alb.tf
@@ -1,5 +1,14 @@
 data "cloudflare_ip_ranges" "cloudflare" {}
 
+resource "aws_acm_certificate" "alb" {
+  domain_name       = "alb.internal.${local.main_domain}"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_security_group" "alb_cloudflare" {
   name   = "${local.project}-alb-cloudflare"
   vpc_id = aws_vpc.main.id
@@ -14,6 +23,20 @@ resource "aws_security_group" "alb_cloudflare" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [for ip in var.admin_ips : "${ip}/32"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = [for ip in var.admin_ips : "${ip}/32"]
   }
@@ -45,8 +68,8 @@ resource "aws_lb_target_group" "app" {
 
 resource "aws_lb_listener" "app" {
   load_balancer_arn = aws_lb.app.arn
-  port              = 80
   protocol          = "HTTP"
+  port              = 80
 
   default_action {
     type             = "forward"
@@ -78,3 +101,24 @@ resource "aws_lb_listener" "app" {
 #    }
 #  }
 #}
+
+resource "aws_lb_listener" "app_https" {
+  load_balancer_arn = aws_lb.app.arn
+  protocol          = "HTTPS"
+  port              = 443
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate.alb.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+
+    ## Maintenance
+    #type = "fixed-response"
+    #fixed_response {
+    #  content_type = "text/html"
+    #  message_body = file("${path.module}/error_page/error.html")
+    #  status_code  = "503"
+    #}
+  }
+}
