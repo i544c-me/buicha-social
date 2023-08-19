c75afa0203e5da6798c89496c0b2adfff87209f3
i544c <vrc@i544c.me>
2023-08-19 21:13:13 +0900

    add: CloudWatch Logs for misskey_output

--- infra/cloudwatch.tf
+++ infra/cloudwatch.tf
@@ -1,3 +1,21 @@
+resource "aws_cloudwatch_log_group" "misskey_output" {
+  name = "misskey_output"
+  retention_in_days = 7
+
+  tags = {
+    Name = "${local.project}-log-output"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "misskey_error" {
+  name = "misskey_error"
+  retention_in_days = 7
+
+  tags = {
+    Name = "${local.project}-log-error"
+  }
+}
+
 resource "aws_s3_bucket" "synthetics_canary" {
   bucket = "${local.project}-synthetics-canary"
 }
