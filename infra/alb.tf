05565252c948b305a9aa25f9955545689d2fef76
i544c <vrc@i544c.me>
2023-10-30 01:28:30 +0900

    feature: Enable maintenance refactoring

--- infra/alb.tf
+++ infra/alb.tf
@@ -75,14 +75,6 @@ resource "aws_lb_listener" "app_https" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
-
-    ## Maintenance
-    #type = "fixed-response"
-    #fixed_response {
-    #  content_type = "text/html"
-    #  message_body = file("${path.module}/error_page/error.html")
-    #  status_code  = "503"
-    #}
   }
 
   depends_on = [
@@ -90,19 +82,39 @@ resource "aws_lb_listener" "app_https" {
   ]
 }
 
-#resource "aws_lb_listener_rule" "maintenance" {
-#  listener_arn = aws_lb_listener.app_https.arn
-#  priority     = 100
-#
-#  action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.app.arn
-#  }
-#
-#  condition {
-#    http_header {
-#      http_header_name = "CF-Connecting-IP"
-#      values           = var.admin_ips
-#    }
-#  }
-#}
+resource "aws_lb_listener_rule" "admin" {
+  listener_arn = aws_lb_listener.app_https.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+
+  condition {
+    http_header {
+      http_header_name = "CF-Connecting-IP"
+      values           = var.admin_ips
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "maintenance" {
+  listener_arn = aws_lb_listener.app_https.arn
+  priority     = 100
+
+  action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/html"
+      message_body = file("${path.module}/error_page/error.html")
+      status_code  = "503"
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
