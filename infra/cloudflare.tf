e56dca5c062c520241c4ef5463035a471beb2467
i544c <131976645+i544c-me@users.noreply.github.com>
2024-01-05 23:54:58 +0900

    chore: Add comments for Cloudflare record (#119)

--- infra/cloudflare.tf
+++ infra/cloudflare.tf
@@ -38,6 +38,7 @@ resource "cloudflare_record" "ses_txt" {
   name    = "_amazonses.${local.main_domain}"
   type    = "TXT"
   value   = aws_ses_domain_identity.buicha_social.verification_token
+  comment = "buicha.social SES"
 }
 
 resource "cloudflare_record" "ses_dkim" {
@@ -47,6 +48,7 @@ resource "cloudflare_record" "ses_dkim" {
   name    = "${each.value}._domainkey.${local.main_domain}"
   type    = "CNAME"
   value   = "${each.value}.dkim.amazonses.com"
+  comment = "buicha.social SES DKIM"
 
   depends_on = [aws_ses_domain_dkim.buicha_social]
 }
@@ -56,6 +58,7 @@ resource "cloudflare_record" "ses_spf" {
   name    = aws_ses_domain_mail_from.buicha_social.mail_from_domain
   type    = "TXT"
   value   = "v=spf1 include:amazonses.com ~all"
+  comment = "buicha.social SES SPF"
 }
 
 resource "cloudflare_record" "ses_mailfrom" {
@@ -64,6 +67,7 @@ resource "cloudflare_record" "ses_mailfrom" {
   type     = "MX"
   priority = 10
   value    = "feedback-smtp.ap-northeast-1.amazonses.com"
+  comment  = "buicha.social SES Feedback"
 }
 
 
@@ -72,6 +76,7 @@ resource "cloudflare_record" "ses_dmark" {
   name    = "_dmarc.${local.main_domain}"
   type    = "TXT"
   value   = "v=DMARC1;p=quarantine;adkim=r;aspf=r;rua=mailto:report@i544c.me;"
+  comment = "buicha.social SES DMARK"
 }
 
 // ACM
@@ -89,4 +94,5 @@ resource "cloudflare_record" "domain_cert_alb" {
   name    = each.value.name
   type    = each.value.type
   value   = each.value.value
+  comment = "buicha.social ALB Cert"
 }
