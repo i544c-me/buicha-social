094bf52d9f9d1649568fc452bb7627e9fdce4c85
i544c <vrc@i544c.me>
2024-06-29 05:59:13 +0900

    chore: 新しいECSサービスに移行

--- v2/infra/alb-misskey-v3.tf
+++ v2/infra/alb-misskey-v3.tf
@@ -58,3 +58,114 @@ resource "aws_lb_target_group" "app_v3" {
     create_before_destroy = true
   }
 }
+
+#resource "aws_lb_listener_rule" "admin" {
+#  listener_arn = aws_lb_listener.app.arn
+#  priority     = 10
+#  action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.app.arn
+#  }
+#  condition {
+#    http_header {
+#      http_header_name = "CF-Connecting-IP"
+#      values           = var.admin_ips
+#    }
+#  }
+#}
+
+#resource "aws_lb_listener_rule" "maintenance" {
+#  listener_arn = aws_lb_listener.app.arn
+#  priority     = 100
+#
+#  action {
+#    type = "fixed-response"
+#    fixed_response {
+#      content_type = "text/html"
+#      message_body = file("${path.module}/bin/error.html")
+#      status_code  = "503"
+#    }
+#  }
+#
+#  condition {
+#    path_pattern {
+#      values = ["/*"]
+#    }
+#  }
+#}
+
+
+### Security group ###
+
+data "cloudflare_ip_ranges" "cloudflare" {}
+
+resource "aws_security_group" "alb_v4" {
+  name   = "${local.project}-alb-v4"
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.project}-alb-v4"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "alb_v4_egress" {
+  security_group_id = aws_security_group.alb_v4.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" # なぜか UDP で通信しているため
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "alb_v6" {
+  name   = "${local.project}-alb-v6"
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.project}-alb-v6"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "alb_v6_ingress" {
+  security_group_id = aws_security_group.alb_v6.id
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  ipv6_cidr_blocks  = data.cloudflare_ip_ranges.cloudflare.ipv6_cidr_blocks
+}
+
+
+resource "aws_security_group_rule" "alb_v6_egress" {
+  security_group_id = aws_security_group.alb_v6.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" # なぜか UDP で通信しているため
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+
+### ACM ###
+
+resource "aws_acm_certificate" "alb" {
+  domain_name       = local.main_domain
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "alb" {
+  certificate_arn         = aws_acm_certificate.alb.arn
+  validation_record_fqdns = [for record in cloudflare_record.domain_cert_alb : record.hostname]
+}
